31b1d25d792f21617c3fb29abde651ad571d9e63
shirshxk <sirshak99@gmail.com>
2024-07-13 16:02:30 +0545

    new

--- try1.py
+++ try1.py
@@ -1,16 +1,32 @@
 import os
+from time import sleep
 
-# Define color constants
+
 GREEN = '\033[32m'
 DEFAULT = '\033[39m'
 ORANGE = '\033[93m'
 RED = '\033[31m'
 
-def clear_screen():
-    os.system('clear')
+def main():
+    clear_screen()
+    welcome_message()
+    devices = get_devices()
+    adapter_index = select_adapter(devices)
+    adapter = devices[adapter_index]
+    clear_screen()
+    networks = get_networks()
+    if not networks:
+        input(f'{RED}No WIFI Networks FOUND{DEFAULT}, Press Enter To Exit')
+        exit()
+    display_networks(networks)
+    network = select_network(networks)
+    clear_screen()
+    start_monitor_mode(adapter)
+    display_network_details(network)
+    flood_network(adapter, network)
 
-def display_ascii_art():
-    print(GREEN + r"""
+def welcome_message():
+    print(GREEN + """
  _    _ _       ______ _                                                  
 | |  | (_)      |  ___(_)                                                 
 | |  | |_ ______| |_   _                                                  
@@ -23,8 +39,10 @@ ______                 _   _                _   _           _
 | | | / _ \/ _` | | | | __| '_ \ / _ \ '_ \| __| |/ __/ _` | __/ _ \| '__|
 | |/ /  __/ (_| | |_| | |_| | | |  __/ | | | |_| | (_| (_| | || (_) | |   
 |___/ \___|\__,_|\__,_|\__|_| |_|\___|_| |_|\__|_|\___\__,_|\__\___/|_|   
-                                                                          """ + DEFAULT)
-    
+                                                                          """)
+    sleep(1)
+
+
 def get_devices():
     cmd = 'ifconfig'
     output = os.popen(cmd).read().split('\n\n')
@@ -47,8 +65,15 @@ def get_networks():
     cmd = 'nmcli --terse -f BSSID,SSID,CHAN,SIGNAL dev wifi'
     output = os.popen(cmd).read().strip().split('\n')
     networks = [line.replace('\\:', '..').split(':') for line in output if line]
-    networks = {i: {'BSSID': n[0].replace('..', ':'), 'SSID': n[1], 'CHANNEL': n[2], 'SIGNAL': n[3]} for i, n in enumerate(networks)}
-    return networks
+    formatted_networks = {}
+    for i, n in enumerate(networks):
+        formatted_networks[i] = {
+            'BSSID': n[0].replace('..', ':'), 
+            'SSID': n[1], 
+            'CHANNEL': n[2], 
+            'SIGNAL': n[3]
+        }
+    return formatted_networks
 
 def display_networks(networks):
     l_ssid = max(len(net['SSID']) for net in networks.values())
@@ -98,29 +123,14 @@ def flood_network(adapter, network):
     finally:
         cleanup(adapter)
 
+def clear_screen():
+    os.system('clear')
+
 def cleanup(adapter):
     clear_screen()
     print(f'Disabling Monitor Mode On {adapter}')
     os.system(f'sudo airmon-ng stop {adapter}mon')
     clear_screen()
-
-def main():
-    clear_screen()
-    display_ascii_art()
-    devices = get_devices()
-    adapter_index = select_adapter(devices)
-    adapter = devices[adapter_index]
-    clear_screen()
-    networks = get_networks()
-    if not networks:
-        input(f'{RED}No WIFI Networks FOUND{DEFAULT}, Press Enter To Exit')
-        exit()
-    display_networks(networks)
-    network = select_network(networks)
-    clear_screen()
-    start_monitor_mode(adapter)
-    display_network_details(network)
-    flood_network(adapter, network)
 
 if __name__ == '__main__':
     try:
@@ -129,3 +139,5 @@ if __name__ == '__main__':
         print(f'{RED}An error occurred: {e}{DEFAULT}')
         if 'adapter' in locals():
             cleanup(adapter)
+
+
