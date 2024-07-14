ef06fb1431c2835f17241ada6b554777c2cd356b
shirshxk <sirshak99@gmail.com>
2024-07-14 14:02:08 +0545

    final commit

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
-#!/usr/bin/python3
 import os
+from time import sleep
 
-# Define color constants
+
 GREEN = '\033[32m'
 DEFAULT = '\033[39m'
 ORANGE = '\033[93m'
 RED = '\033[31m'
 
-def clear_screen():
-    os.system('clear')
+def main():
+    clear_screen()
+    welcome_message()
+    devices = get_devices()
+    adapter_index = select_adapter(devices)
+    adapter = devices[adapter_index]
+    clear_screen()
+    networks = get_networks()
+    if not networks:
+        input(f'{RED}No WIFI Networks FOUND{DEFAULT}, Press Enter To Exit')
+        exit()
+    display_networks(networks)
+    network = select_network(networks)
+    clear_screen()
+    start_monitor_mode(adapter)
+    display_network_details(network)
+    flood_network(adapter, network)
+
+def welcome_message():
+    print(GREEN + """
+ _    _ _       ______ _                                                  
+| |  | (_)      |  ___(_)                                                 
+| |  | |_ ______| |_   _                                                  
+| |/\| | |______|  _| | |                                                 
+\  /\  / |      | |   | |                                                 
+ \/  \/|_|      \_|   |_|                                                 
+______                 _   _                _   _           _             
+|  _  \               | | | |              | | (_)         | |            
+| | | |___  __ _ _   _| |_| |__   ___ _ __ | |_ _  ___ __ _| |_ ___  _ __ 
+| | | / _ \/ _` | | | | __| '_ \ / _ \ '_ \| __| |/ __/ _` | __/ _ \| '__|
+| |/ /  __/ (_| | |_| | |_| | | |  __/ | | | |_| | (_| (_| | || (_) | |   
+|___/ \___|\__,_|\__,_|\__|_| |_|\___|_| |_|\__|_|\___\__,_|\__\___/|_|   
+                                                                          """)
+    sleep(1)
+
 
 def get_devices():
     cmd = 'ifconfig'
@@ -32,15 +65,23 @@ def get_networks():
     cmd = 'nmcli --terse -f BSSID,SSID,CHAN,SIGNAL dev wifi'
     output = os.popen(cmd).read().strip().split('\n')
     networks = [line.replace('\\:', '..').split(':') for line in output if line]
-    networks = {i: {'BSSID': n[0].replace('..', ':'), 'SSID': n[1], 'CHANNEL': n[2], 'SIGNAL': n[3]} for i, n in enumerate(networks)}
-    return networks
+    formatted_networks = {}
+    for i, n in enumerate(networks):
+        formatted_networks[i] = {
+            'BSSID': n[0].replace('..', ':'), 
+            'SSID': n[1], 
+            'CHANNEL': n[2], 
+            'SIGNAL': n[3]
+        }
+    return formatted_networks
 
 def display_networks(networks):
     l_ssid = max(len(net['SSID']) for net in networks.values())
     print(f"NO.    BSSID{' ' * 16}SSID{' ' * (l_ssid)}SIG{' ' * 4}CHANNEL\n{'-' * (l_ssid + 46)}")
     for i, net in networks.items():
         signal_color = get_signal_color(int(net['SIGNAL']))
-        print(f"{i:<3}    {net['BSSID']}    {net['SSID']:<{l_ssid}}    {signal_color}{net['SIGNAL']:<3}{DEFAULT}    {net['CHANNEL']}")
+        print(f"{i:<3}    {net['BSSID']}    {net['SSID']:<{l_ssid}}", end="")
+        print("    {signal_color}{net['SIGNAL']:<3}{DEFAULT}    {net['CHANNEL']}")
 
 def get_signal_color(signal_strength):
     if signal_strength > 50:
@@ -84,38 +125,16 @@ def flood_network(adapter, network):
     finally:
         cleanup(adapter)
 
+def clear_screen():
+    os.system('clear')
+
 def cleanup(adapter):
     clear_screen()
     print(f'Disabling Monitor Mode On {adapter}')
-    os.system(f'sudo airmon-ng stop {adapter}mon ')
-    os.system(f'sudo ifconfig {adapter} down')
-    os.system(f'sudo iwconfig {adapter} mode managed')
-    os.system(f'sudo ifconfig {adapter} up')
-    os.system('sudo service NetworkManager restart')
+    os.system(f'sudo airmon-ng stop {adapter}mon')
     clear_screen()
-
-
-def main():
-    clear_screen()
-    devices = get_devices()
-    adapter_index = select_adapter(devices)
-    adapter = devices[adapter_index]
-    clear_screen()
-    networks = get_networks()
-    if not networks:
-        input(f'{RED}No WIFI Networks FOUND{DEFAULT}, Press Enter To Exit')
-        exit()
-    display_networks(networks)
-    network = select_network(networks)
-    clear_screen()
-    start_monitor_mode(adapter)
-    display_network_details(network)
-    flood_network(adapter, network)
 
 if __name__ == '__main__':
-    try:
-        main()
-    except Exception as e:
-        print(f'{RED}An error occurred: {e}{DEFAULT}')
-        if 'adapter' in locals():
-            cleanup(adapter)
+    main()
+
+
